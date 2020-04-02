a5715b5bf7a7b8a37809bf61463704e8009c2ef4
Les Wright <lesliewright1977@gmail.com>
2020-04-02 10:12:56 +0100

    Add files via upload

---
+++ src/displaythermal.py
@@ -0,0 +1,100 @@
+import serial
+import numpy as np
+import cv2
+import math
+
+
+ser = serial.Serial("/dev/ttyACM0", 115200)
+
+if ser.is_open == False:
+	ser.open()
+
+nmin = 0
+nmax = 255
+alpha1 = 0.5
+alpha2 = 0.5
+
+frames = 0
+index = 0
+temp = 0
+while True:
+	frames+=1
+	recv = ser.readline()
+	recv = recv.rstrip() #strip the return character
+
+	#next job, split on , stick the data in an array
+	data = np.fromstring(recv, dtype=float, count=-1, sep=',') #get the data
+	#print(data)
+
+
+	heatmap = np.zeros((24,32,3), np.uint8) #create the blank image to work from
+	#add to the image
+	index = 0
+	if len(data) == 768: #Check we have good data!
+		for y in range (0,24):
+			for x in range (0,32):
+				val = (data[index]*10)-100
+				if math.isnan(val):
+					val = 0
+				if val > 255:
+					val=255
+				#print(index)
+				#print(data)
+
+				heatmap[y,x] = (val,val,val)
+	
+				if(y == 12) and (x == 16):
+					temp = data[index]
+
+				index+=1
+
+	heatmap = cv2.rotate(heatmap, cv2.ROTATE_90_COUNTERCLOCKWISE)
+	heatmap = cv2.flip(heatmap, 1 ) #flip heatmap to match image
+	heatmap = cv2.normalize(heatmap,None,nmin,nmax,cv2.NORM_MINMAX)
+	heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+	heatmap = cv2.resize(heatmap,(240,320),interpolation=cv2.INTER_CUBIC)
+
+
+	# Display the resulting frame
+	cv2.namedWindow('Thermal',cv2.WINDOW_NORMAL)
+	# display black box for our data
+	cv2.rectangle(heatmap, (0, 0),(90, 15), (0,0,0), -1)
+	# put text in the box
+	cv2.putText(heatmap,'Temp: '+str(temp), (10, 10),\
+	cv2.FONT_HERSHEY_SIMPLEX, 0.3,(0, 255, 255), 1, cv2.LINE_AA)
+
+	# draw crosshairs
+	cv2.line(heatmap,(120,150),(120,170),(0,0,0),1) #vline
+	cv2.line(heatmap,(110,160),(130,160),(0,0,0),1) #hline
+
+
+	cv2.imshow('Thermal',heatmap)
+
+	res = cv2.waitKey(1)
+	#print(res)
+
+	if res == 113: #q
+		break
+	if res == 97: #a
+		nmin += 10
+		print(nmin)
+	if res == 122: #z
+		nmin -= 10
+		print(nmin)
+	if res == 115: #s
+		nmax += 10
+		print(nmax)
+	if res == 120: #x
+		nmax -= 10
+		print(nmax)
+	if res == 100: #d
+		alpha1 += 0.1
+		alpha2 -= 0.1
+	if res == 99: #c
+		alpha1 -= 0.1
+		alpha2 += 0.1
+
+
+
+cv2.destroyAllWindows()
+
