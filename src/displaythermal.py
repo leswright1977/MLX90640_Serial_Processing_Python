419d6c0a9d1aba5596bcd00aec2f8a5cf25f047b
Les Wright <lesliewright1977@gmail.com>
2020-04-03 09:24:54 +0100

    Add files via upload

--- src/displaythermal.py
+++ src/displaythermal.py
@@ -15,16 +15,18 @@ nmax = 255
 frames = 0
 index = 0
 temp = 0
+
+alpha = -1.3 # Contrast control (1.0-3.0)
+beta = 92 # Brightness control (0-100)
+rad = 2
+
 while True:
 	frames+=1
 	recv = ser.readline()
 	recv = recv.rstrip() #strip the return character
-
 	#next job, split on , stick the data in an array
 	data = np.fromstring(recv, dtype=float, count=-1, sep=',') #get the data
 	#print(data)
-
-
 	heatmap = np.zeros((24,32,3), np.uint8) #create the blank image to work from
 	#add to the image
 	index = 0
@@ -32,10 +34,11 @@ while True:
 		for y in range (0,24):
 			for x in range (0,32):
 				val = (data[index]*10)-100
+				#print(val)
 				if math.isnan(val):
 					val = 0
-				if val > 255:
-					val=255
+				#if val > 255:
+				#	val=255
 				#print(index)
 				#print(data)
 
@@ -48,9 +51,13 @@ while True:
 
 	heatmap = cv2.rotate(heatmap, cv2.ROTATE_90_CLOCKWISE)#rotate
 	heatmap = cv2.flip(heatmap, 1 ) #flip heatmap
-	heatmap = cv2.normalize(heatmap,None,nmin,nmax,cv2.NORM_MINMAX)
-	heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-	heatmap = cv2.resize(heatmap,(240,320),interpolation=cv2.INTER_CUBIC)
+	#heatmap = cv2.normalize(heatmap,None,nmin,nmax,cv2.NORM_MINMAX)
+	heatmap = cv2.convertScaleAbs(heatmap, alpha=alpha, beta=beta)#Bright/Contrast
+
+	heatmap = cv2.blur(heatmap,(rad,rad))
+
+	heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)#apply colormap
+	heatmap = cv2.resize(heatmap,(240,320),interpolation=cv2.INTER_CUBIC)#Scale up!
 
 
 	# Display the resulting frame
@@ -74,17 +81,20 @@ while True:
 	if res == 113: #q
 		break
 	if res == 97: #a
-		nmin += 10
-		print(nmin)
+		rad += 1
 	if res == 122: #z
-		nmin -= 10
-		print(nmin)
+		if rad == 1:
+			rad = 1
+		else:
+			rad -= 1
+	if res == 100: #d
+		alpha += 0.1
+	if res == 99: #c
+		alpha -= 0.1
 	if res == 115: #s
-		nmax += 10
-		print(nmax)
+		beta += 1
 	if res == 120: #x
-		nmax -= 10
-		print(nmax)
+		beta -= 1
 
 
 
